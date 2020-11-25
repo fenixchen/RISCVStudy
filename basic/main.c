969f50e9ae635543d26600355738d891f1cb496e
Chen Changhong <12888022@qq.com>
2020-11-25 16:57:14 +0800

    UART OK

--- basic/main.c
+++ basic/main.c
@@ -1,7 +1,18 @@
+#include "uart.h"
+
+
 int main(){
 	int a = 10;
 	int b = 20;
 	int c = 0;
+
+	uart_init();
+	uart_putc('A');
+	uart_putc('B');
+	uart_putc('C');
+	uart_putc('D');
+	uart_putc('\n');
+
 	while (1){
 		c = a + b;
 	}
