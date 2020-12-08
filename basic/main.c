4ef7ee7493d0fe0f9cfb08d3b2bd73f542a0d043
Chen Changhong <12888022@qq.com>
2020-12-08 13:55:53 +0800

    format code

--- basic/main.c
+++ basic/main.c
@@ -5,20 +5,12 @@
 void kernelvec();
 
 int main(){
-	int a = 10;
-	int b = 20;
-	int c = 0;
-
 	uart_init();
 
     w_stvec((uint64_t)kernelvec);
 
-    uart_puts("Hello RISCV\n");
+    puts("Hello RISCV\n");
 
-	while (1){
-		c = a + b;
-	}
-	while (c--){
-	}
+    puts(">> Done\n");
 	return 0;
 }
